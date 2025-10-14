baa77e873f832005fdc6b17bcc0e40836b7e11f7
Walacy Pereira <143743509+WollygrinX@users.noreply.github.com>
2025-10-14 00:25:51 -0300

    Atividade 5

---
+++ src/Main.java
@@ -0,0 +1,38 @@
+import java.util.Random;
+import java.util.Scanner;
+
+
+public class Main {
+    public static void main(String[] args) {
+
+
+        //gerando números aletorios
+        Random gerador = new Random();
+
+        int numero_maquina = gerador.nextInt(51);
+        int tentativa;
+
+        //entrada de dados
+        Scanner entrada = new Scanner(System.in);
+        System.out.println("Adivinhe o número entre 0 e 50: ");
+        //leia
+        tentativa = entrada.nextInt();
+
+        //enquanto tentativa não for igual a número da máquina
+        while (tentativa != numero_maquina){
+            //se tentativa for < que maquina
+            if (tentativa < numero_maquina) {
+                System.out.println("O número é maior!");
+                //
+            } else {
+                System.out.println("O número é menor!");
+            }
+            System.out.println("Tente novamente: ");
+            tentativa = entrada.nextInt();
+        }
+        System.out.println("Parabéns, você acertou o número!!");
+
+         entrada.close();
+    //FIM DO MEU CODIGO
+    }
+}
